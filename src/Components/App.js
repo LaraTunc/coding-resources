3c03b2fbc62c374fea92513c1b077d8e33c7ec3a
Lara Tunc <laratunc@gmail.com>
2021-05-29 16:51:51 -0400

    complete

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import GlobalStyles from './GlobalStyles';
+import Modal from './Modal';
+
+const iconWidth = 250;
+const iconLeft = (window.innerWidth - iconWidth) / 2;
 
 const App = () => {
-  const [data, setData] = useState();
-  const url = 'https://api.sampleapis.com/coffee/hot';
+  const [codingResources, setCodingResources] = useState();
+  const [selectedResource, setSelectedResource] = useState();
+  const url = 'https://api.sampleapis.com/codingresources/codingResources';
 
   useEffect(() => {
-    // this is where we fetch the data from the server and add it to state.
     fetch(url)
       .then((res) => res.json())
-      .then((json) => setData(json))
+      .then((json) => setCodingResources(json))
       .catch((err) => console.error(err));
   }, []);
 
-  console.log(data);
-
   return (
     <Wrapper>
-      <header className='App-header'>
-        <p>Hello</p>
-      </header>
+      {selectedResource && (
+        <Modal
+          selectedResource={selectedResource}
+          setSelectedResource={setSelectedResource}
+        />
+      )}
+      <Content opacity={selectedResource ? '20%' : '100%'}>
+        <GlobalStyles />
+        <Header>
+          <Img src='../.././logo.png' alt='logo' />
+        </Header>
+        <Section>
+          <Img2 src='../.././computer.png' alt='coffee machine' />
+          {codingResources &&
+            codingResources.map((resource) => (
+              <Item
+                key={resource.id}
+                onClick={() => setSelectedResource(resource)}
+              >
+                {resource.description}
+              </Item>
+            ))}
+        </Section>
+      </Content>
     </Wrapper>
   );
 };
@@ -27,5 +51,57 @@ const App = () => {
 export default App;
 
 const Wrapper = styled.div`
-  border: 1px solid black;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  border: 25px solid #2255ff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  opacity: ${({ opacity }) => opacity};
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  width: 50%;
+  border-bottom: 2px solid white;
+`;
+
+const Img = styled.img`
+  width: 200px;
+  margin: 30px;
+`;
+
+const Section = styled.section`
+  padding: 10px;
+  padding-top: 270px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  position: relative;
+`;
+
+const Img2 = styled.img`
+  width: ${iconWidth}px;
+  position: absolute;
+  top: 25px;
+  left: ${iconLeft}px;
+`;
+
+const Item = styled.button`
+  margin: 20px;
+  border: none;
+  width: 300px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  &:hover {
+    color: #ffd558;
+  }
 `;
---
+++ src/Components/Modal.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Modal = ({ selectedResource, setSelectedResource }) => {
+  const formatArrayContentToString = (array) => {
+    const formatted = array.map((item, index) => {
+      if (index === array.length - 1) {
+        return item;
+      } else {
+        return `${item}, `;
+      }
+    });
+    return formatted;
+  };
+
+  return (
+    <Wrapper>
+      <Button onClick={() => setSelectedResource()}>X</Button>
+      <Content>
+        <Title>{selectedResource.description}</Title>
+        <Info>URL: {selectedResource.url}</Info>
+        <Info>
+          Resource type: {formatArrayContentToString(selectedResource.types)}
+        </Info>
+        <Info>
+          Topics: {formatArrayContentToString(selectedResource.topics)}
+        </Info>
+        <Info>
+          Levels: {formatArrayContentToString(selectedResource.levels)}
+        </Info>
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Modal;
+
+const Wrapper = styled.div`
+  border: 25px solid #2255ff;
+  background: #17161c;
+  max-width: 100vw;
+  min-width: 70vw;
+  margin-top: 40px;
+  position: fixed;
+  z-index: 5;
+`;
+
+const Button = styled.button`
+  margin: 10px;
+  border: none;
+  font-family: sans-serif;
+  &:hover {
+    color: #ffd558;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  padding: 10px;
+  margin: 10px;
+  border-bottom: 1px solid white;
+`;
+
+const Info = styled.div`
+  padding: 20px;
+  line-height: 1.5;
+`;
